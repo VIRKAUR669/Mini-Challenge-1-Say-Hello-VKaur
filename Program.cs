17e21ce3b7db9824dbc0d0b887f652a2c82d26fe
VIRKAUR669 <virpal.kaur.rai@gmail.com>
2026-09-08 16:10:33 -0700

    remove git

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Hello, World!");
+//Virpal Kaur
+//Mini Challeneg One Say Hello
+//I use a variable to save a user"s name 
+//Peer Reviwe name;
+//Review
+
+Console.Write("Virpal Kaur");
+//test
+Console.Clear();
